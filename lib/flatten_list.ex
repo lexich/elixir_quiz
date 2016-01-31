632218fb9a071bdcdfcf2ba7ee40d46e6bfd76ff
Efremov Alexey <lexich121@gmail.com>
2016-01-31 13:31:15 +0300

    Add: more solutions

--- lib/flatten_list.ex
+++ lib/flatten_list.ex
@@ -6,15 +6,23 @@ defmodule FlattenList do
     [1,2,3,4,5,6]
 
     """
-    def flatten([]) do 
-        []
-    end
-    def flatten(array) when is_list(array) do
-        [head | tail] = array
+    def flatten([]), do: []
+    def flatten(item) when is_integer(item), do: [item]
+    def flatten(list) when is_list(list) do
+        [head | tail] = list
         flatten(head) ++ flatten(tail)
     end
 
-    def flatten(item) when is_integer(item) do        
-        [item]
-    end    
+    # https://forums.pragprog.com/forums/322/topics/Exercise:%20ListsAndRecursion-6
+    def flatten2([head | tail]), do: flatten2(head) ++ flatten2(tail)
+    def flatten2([]), do: []        
+    def flatten2(head), do: [head]
+
+    
+    def flatten3(list), do: _flatten3(list, [])
+    defp _flatten3([], result), do: Enum.reverse(result)
+    defp _flatten3([[] | tail], result), do: _flatten3(tail, result)
+    defp _flatten3([ [ h | [] ] | tail], result), do: _flatten3([ h | tail], result)    
+    defp _flatten3([ [ h | t ] | tail], result), do: _flatten3([ h, t | tail], result)
+    defp _flatten3([ head | tail ], result), do: _flatten3(tail, [ head | result ])
 end
--- test/flatten_list_test.exs
+++ test/flatten_list_test.exs
@@ -2,12 +2,32 @@ defmodule FlattenListTest do
   use ExUnit.Case
   doctest FlattenList
 
-  test "flatten list flatten" do
+  test "flatten list" do
     assert FlattenList.flatten(
         [ 1, [ 2, 3, [4] ], 5, [[[6]]]]
     ) == [1, 2, 3, 4, 5, 6]
-    
+
     assert FlattenList.flatten(
+        [ [], 1, [ 2, 3, [4] ], 5, [[[6]]]]
+    ) == [1, 2, 3, 4, 5, 6]
+  end
+
+  test "flatten2 list" do
+    assert FlattenList.flatten2(
+        [ 1, [ 2, 3, [4] ], 5, [[[6]]]]
+    ) == [1, 2, 3, 4, 5, 6]
+    
+    assert FlattenList.flatten2(
+        [ [], 1, [ 2, 3, [4] ], 5, [[[6]]]]
+    ) == [1, 2, 3, 4, 5, 6]
+  end
+
+  test "flatten3 list" do
+    assert FlattenList.flatten3(
+        [ 1, [ 2, 3, [4] ], 5, [[[6]]]]
+    ) == [1, 2, 3, 4, 5, 6]
+    
+    assert FlattenList.flatten3(
         [ [], 1, [ 2, 3, [4] ], 5, [[[6]]]]
     ) == [1, 2, 3, 4, 5, 6]
   end
