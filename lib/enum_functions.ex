13bdb2bb0ee39769768549383e7d2bc6760ee9d0
Efremov Alexey <lexich121@gmail.com>
2016-01-31 16:41:11 +0300

    Refactoring

--- lib/enum_functions.ex
+++ lib/enum_functions.ex
@@ -3,56 +3,42 @@ defmodule EnumFunctions do
     Implement the following Enum functions using no library functions or list comprehensions: all?, each, filter, split, and take. You may need to use an if statement to implement filter. The syntax for this is
     if condition do
       expression(s)
-    else         
+    else
       expression(s)
     end
     """
     def all?([head | tail], func) do
-        if func.(head) do
-            all?(tail, func)
-        else
-            false
-        end
+      if func.(head), do: all?(tail, func), else: false
     end
     def all?([], _), do: true
 
-    def each([head | tail], func) do
-        [func.(head) | each(tail, func)]
-    end
+    def each([head | tail], func), do: [func.(head) | each(tail, func)]
     def each([], _), do: []
 
-    def filter([head | tail], func) do 
-        if func.(head) do
-            [head | filter(tail, func)]
-        else
-            filter(tail, func)
-        end
+    def filter([head | tail], func) do
+      if func.(head) do
+        [head | filter(tail, func)]
+      else
+        filter(tail, func)
+      end
     end
     def filter([], _), do: []
 
     def reverse(list), do: _reverse(list, [])
-    defp _reverse([head | tail], result), do: _reverse(tail, [head | result])    
+    defp _reverse([head | tail], result), do: _reverse(tail, [head | result])
     defp _reverse([], result), do: result
 
-
-    def split(list, num) when num < 0 do        
-        r_list = reverse(list)
-        [first, last] = split r_list, (-1 * num)
-        [reverse(last), reverse(first)]        
+    def split(list, num) when num < 0 do
+      [first, last] = split reverse(list), (-1 * num)
+      [reverse(last), reverse(first)]
     end
     def split(list, num), do: _split(list, num, [])
-    defp _split([], num, result), do: [reverse(result), []]
+    defp _split([], _, result), do: [reverse(result), []]
     defp _split(list, 0, result), do: [reverse(result), list]
-    defp _split([head | tail], num, result) do
-        _split tail, num - 1, [head | result]
-    end
+    defp _split([head | tail], num, result), do: _split tail, num - 1, [head | result]
 
     def take([], _), do: []
     def take(_, 0), do: []
-    def take([head | tail], num) when num > 0, do: [head | take(tail, num - 1)]        
-    def take(list, num) do
-        r_list = reverse list
-        result = take r_list, (-1 * num)
-        reverse result
-    end
+    def take([head | tail], num) when num > 0, do: [head | take(tail, num - 1)]
+    def take(list, num), do: reverse(take reverse(list), (-1 * num))
 end
--- lib/flatten_list.ex
+++ lib/flatten_list.ex
@@ -4,25 +4,24 @@ defmodule FlattenList do
 
     iex> FlattenList.flatten([ 1, [ 2, 3, [4] ], 5, [[[6]]]])
     [1,2,3,4,5,6]
-
     """
     def flatten([]), do: []
     def flatten(item) when is_integer(item), do: [item]
     def flatten(list) when is_list(list) do
-        [head | tail] = list
-        flatten(head) ++ flatten(tail)
+      [head | tail] = list
+      flatten(head) ++ flatten(tail)
     end
 
     # https://forums.pragprog.com/forums/322/topics/Exercise:%20ListsAndRecursion-6
     def flatten2([head | tail]), do: flatten2(head) ++ flatten2(tail)
-    def flatten2([]), do: []        
+    def flatten2([]), do: []
     def flatten2(head), do: [head]
 
-    
+
     def flatten3(list), do: _flatten3(list, [])
     defp _flatten3([], result), do: Enum.reverse(result)
     defp _flatten3([[] | tail], result), do: _flatten3(tail, result)
-    defp _flatten3([ [ h | [] ] | tail], result), do: _flatten3([ h | tail], result)    
+    defp _flatten3([ [ h | [] ] | tail], result), do: _flatten3([ h | tail], result)
     defp _flatten3([ [ h | t ] | tail], result), do: _flatten3([ h, t | tail], result)
     defp _flatten3([ head | tail ], result), do: _flatten3(tail, [ head | result ])
 end
